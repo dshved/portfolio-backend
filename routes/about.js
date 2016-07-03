6c8d7433acc3fe5a455f5f7a98db2c552df46e94
dshved <dshved@hotmail.com>
2016-07-03 13:46:17 +0300

    Исправлена работа при отсутствии данных в БД

--- routes/about.js
+++ routes/about.js
@@ -5,7 +5,23 @@ var Skill = require('../models/skills').Skill;
 
 router.get('/', function(req, res, next) {
   Skill.find().then(function(skills){
-    res.render('about', { data: skills[0] });
+
+  	if (skills.length < 1) {
+
+			var data = {
+				html: 0, css: 0, js: 0, 
+				git: 0, gulp: 0, bower: 0, 
+				php: 0, mysql: 0, nodejs: 0, mongodb:0	
+			};
+
+			res.render('about', {data: data});
+
+		} else {
+
+			res.render('about', { data: skills[0] });	
+
+		};
+
   });
   
 });
--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,17 @@ var Skill = require('../models/skills').Skill;
 
 router.get('/', function(req, res, next) {
 	Skill.find().then(function(skills){
-    res.render('admin', { data: skills[0] });
+		if (skills.length < 1) {
+			var data = {
+				html: 0, css: 0, js: 0, 
+				git: 0, gulp: 0, bower: 0, 
+				php: 0, mysql: 0, nodejs: 0, mongodb:0	
+			};
+			res.render('admin', {data: data});
+		} else {
+			res.render('admin', { data: skills[0] });	
+		}
+    
   });
 });
 
